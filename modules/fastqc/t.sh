77dbf4aac4d2fe3ede48dbd0a5b4a4453860454a
jinn0027 <jinn@zb3.so-net.ne.jp>
2025-01-17 00:11:14 +0900

    .

--- modules/fastqc/t.sh
+++ modules/fastqc/t.sh
@@ -9,5 +9,5 @@ fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
 odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
 
 mkdir -p ${odir}
-/usr/local/bin/singularity exec --fakeroot --bind ${fq1},${fq2},${odir} fastqc.sif fastqc -o ${odir} ${fq1} ${fq2}
+/usr/local/bin/apptainer exec --fakeroot --bind ${fq1},${fq2},${odir} fastqc.sif fastqc -o ${odir} ${fq1} ${fq2}
 
