ab9c732f61f5b1e6034b5e8a4cf594639d753a4e
jinn0027 <jinn@zb3.so-net.ne.jp>
2025-01-21 12:43:01 +0900

    make test template

--- modules/tmp/test/t.sh
+++ modules/tmp/test/t.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-fq1=../test/s_6_1.fastq.gz
-fq2=../test/s_6_2.fastq.gz
+fq1=../../test/s_6_1.fastq.gz
+fq2=../../test/s_6_2.fastq.gz
+
 odir=results
+refdir=ref
+
+log=t.log
+
+ret=0
 
 fq1=$(cd $(dirname ${fq1}) && pwd)/$(basename ${fq1})
 fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
 odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
 
 mkdir -p ${odir}
-/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} SSS.sif SSS -h
+/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../SSS.sif SSS -h > ${log} 2>&1
+for i in $(ls $odir/*.html)
+do
+    j=$refdir/$(basename $i)
+    diff -q $i $j >> ${log} 2>&1 && :
+    if [ $? -ne 0 ]; then
+        ret=1
+    fi
+done
+
+if [ ${ret} -eq 0 ]; then
+    echo " PASSED" | tee -a ${log}
+else
+    echo " FAILED" | tee -a ${log}
+fi
+
+exit ${ret}
 
 
 
