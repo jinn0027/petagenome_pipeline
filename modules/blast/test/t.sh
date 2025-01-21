698c045e1f4fae9a8eba5b5c2cb0a391da9d5b51
jinn0027 <jinn@zb3.so-net.ne.jp>
2025-01-21 23:55:23 +0900

    add blast test

--- modules/blast/test/t.sh
+++ modules/blast/test/t.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-fq1=../../test/s_6_1.fastq.gz
-fq2=../../test/s_6_2.fastq.gz
+n_threads=$(nproc)
+n_al="1"
+pi_thre="95"
+e_thre="1e-20"
+
+fa1=../../test/ecoli_1K_1.fa.gz
+fa2=../../test/q.fa
+
+wdir=work
 odir=results
 refdir=ref
 
@@ -9,23 +16,38 @@ log=t.log
 
 ret=0
 
-fq1=$(cd $(dirname ${fq1}) && pwd)/$(basename ${fq1})
-fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
+fa1=$(cd $(dirname ${fa1}) && pwd)/$(basename ${fa1})
+fa2=$(cd $(dirname ${fa2}) && pwd)/$(basename ${fa2})
+wdir=$(cd $(dirname ${wdir}) && pwd)/$(basename ${wdir})
 odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
 
-mkdir -p ${odir}
-/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../ncbi-blast.sif ncbi-blast -h > ${log} 2>&1
+mkdir -p ${odir} ${wdir}
+
+wfa1=${wdir}/$(basename ${fa1} | sed 's#.gz$##')
+db=${wdir}/$(basename ${fa1})
+
+gunzip ${fa1} -c > ${wfa1}
+
+/usr/local/bin/apptainer exec --bind ${wdir} ../blast.sif makeblastdb -in ${wfa1} -out ${db} -dbtype nucl -parse_seqids > ${log} 2>&1
+
+/usr/local/bin/apptainer exec --bind ${wdir},${fa2},${odir} ../blast.sif blastn -task megablast -num_threads ${n_threads} -query ${fa2} -db ${db} -perc_identity ${pi_thre} -evalue ${e_thre} -outfmt 6 -num_alignments ${n_al} -out ${odir}/out.txt >> ${log} 2>&1
 
 failed=""
-for i in $(ls ${odir}/*.fa)
-do
-    j=${refdir}/$(basename $i)
-    diff -q $i $j >> ${log} 2>&1 && :
-    if [ $? -ne 0 ]; then
-	failed="${failed} $(basename $i)"
-        ret=1
-    fi
-done
+n=$(ls results/*.txt 2> /dev/null | wc -l)
+if [ $n -gt 0 ] ; then
+    for i in $(ls ${odir}/*.txt)
+    do
+        j=${refdir}/$(basename $i)
+        diff -q $i $j >> ${log} 2>&1 && :
+        if [ $? -ne 0 ]; then
+            failed="${failed} $(basename $i)"
+            ret=1
+        fi
+    done
+else
+    failed="no result found"
+    ret=1
+fi
 
 if [ ${ret} -eq 0 ]; then
     echo " PASSED" | tee -a ${log}
@@ -36,7 +58,7 @@ fi
 exit ${ret}
 
 # #{blast_dir}/makeblastdb -in #{out}.1000.fa -out #{out}.1000 -dbtype nucl -parse_seqids
-# #{blast_dir}/bin/blastn -task #{task} -num_threads #{n_threads} -query #{prophage}.fa -db #{contig_proph} -perc_identity #{pi_thre} -evalue #{e_thre} -outfmt 6 -num_alignments #{n_al} -out #{blast_out}.tx
+# #{blast_dir}/bin/blastn -task megablast -num_threads #{n_threads} -query #{prophage}.fa -db #{contig_proph} -perc_identity #{pi_thre} -evalue #{e_thre} -outfmt 6 -num_alignments #{n_al} -out #{blast_out}.tx
 # 03_assembly.rb 04_pool_contigs.rb 05_circular_contigs.rb 06_prophage_detection.02_after_processing.rb
 
 
