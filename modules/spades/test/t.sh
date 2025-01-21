a6f3348430fd5b62247c40e01c2e9495f05c4fbf
jinn0027 <jinn@zb3.so-net.ne.jp>
2025-01-21 14:53:52 +0900

    .

--- modules/spades/test/t.sh
+++ modules/spades/test/t.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-n_threads=$(nproc)
+#n_threads=$(nproc)
+n_threads=4
+# Since #theads affects the results of spades, it's necessary to fix them.
 mem=128
 
-fq1=../../test/s_6_1.fastq.gz
-fq2=../../test/s_6_2.fastq.gz
+fq1=../../test/ecoli_1K_1.fq.gz
+fq2=../../test/ecoli_1K_2.fq.gz
 
+wdir=work
 odir=results
 refdir=ref
 
@@ -17,11 +20,26 @@ fq1=$(cd $(dirname ${fq1}) && pwd)/$(basename ${fq1})
 fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
 odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
 
-mkdir -p ${odir}
+mkdir -p ${odir} ${wdir}
 /usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../spades.sif spades.py --only-error-correction --pe1-1 ${fq1} --pe1-2 ${fq2} --memory ${mem} --threads ${n_threads} -o ${odir} > ${log} 2>&1
-for i in $(ls ${odir}/corrected "*.fastq.gz")
+
+/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../spades.sif spades.py --only-assembler --pe1-1 ${fq1} --pe1-2 ${fq2} --meta --memory ${mem} --threads ${n_threads} -o ${odir} >> ${log} 2>&1
+
+for i in $(ls ${odir}/corrected/*.fastq.gz)
+do
+    gunzip $i
+done
+for i in $(ls ${odir}/corrected/*.fastq)
 do
     j=${refdir}/corrected/$(basename $i)
+    diff -q $i $j >> ${log} 2>&1 && :
+    if [ $? -ne 0 ]; then
+        ret=1
+    fi
+done
+for i in $(ls ${odir}/scaffolds.fasta)
+do
+    j=${refdir}/$(basename $i)
     diff -q $i $j >> ${log} 2>&1 && :
     if [ $? -ne 0 ]; then
         ret=1
