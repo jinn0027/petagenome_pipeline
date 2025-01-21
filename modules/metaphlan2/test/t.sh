94b6f18daba5d9f6836e08266a34cb5f44900d3e
jinn0027 <jinn@zb3.so-net.ne.jp>
2025-01-22 04:50:03 +0900

    add metaphlan2 test

--- modules/metaphlan2/test/t.sh
+++ modules/metaphlan2/test/t.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-fq1=../../test/s_6_1.fastq.gz
-fq2=../../test/s_6_2.fastq.gz
+n_threads=$(nproc)
 
+fq1=../../test/s_6_1.fastq.gz
+db=/opt/MetaPhlAn2/db_v20/mpa_v20_m200
+
+wdir=work
 odir=results
 refdir=ref
 
@@ -11,18 +14,29 @@ log=t.log
 ret=0
 
 fq1=$(cd $(dirname ${fq1}) && pwd)/$(basename ${fq1})
-fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
 
+wdir=$(cd $(dirname ${wdir}) && pwd)/$(basename ${wdir})
 odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
 
-mkdir -p ${odir}
-/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../fastqc.sif fastqc -o ${odir} ${fq1} ${fq2} > ${log} 2>&1
+mkdir -p ${odir} ${wdir}
+/usr/local/bin/apptainer exec --bind ${fq1},${odir} ../metaphlan2.sif sh -c "zcat ${fq1} | metaphlan2.py --bowtie2db ${db} --input_type fastq --bowtie2out ${odir}/out.sam --nproc ${n_threads} > ${odir}/out.prof" > ${log} 2>&1
 
 failed=""
-for i in $(ls ${refdir}/*.fa)
+for i in $(ls ${refdir}/*.prof)
 do
     j=${odir}/$(basename $i)
     diff -q $i $j >> ${log} 2>&1 && :
+    if [ $? -ne 0 ]; then
+        failed="${failed} $(basename $i)"
+        ret=1
+    fi
+done
+for i in $(ls ${refdir}/*.sam)
+do
+    j=${odir}/$(basename $i)
+    head -n 100 $j > ${wdir}/$(basename $i)
+    tail -n 100 $j >> ${wdir}/$(basename $i)
+    diff -q $i ${wdir}/$(basename $i) >> ${log} 2>&1 && :
     if [ $? -ne 0 ]; then
         failed="${failed} $(basename $i)"
         ret=1
@@ -37,30 +51,7 @@ fi
 
 exit ${ret}
 
-fq1=$(cd $(dirname ${fq1}) && pwd)/$(basename ${fq1})
-fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
-odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
-
-mkdir -p ${odir}
-/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../metaphlan2.sif metaphlan2.py --help > ${log} 2>&1
-for i in $(ls $odir/*.html)
-do
-    j=${refdir}/$(basename $i)
-    diff -q $i $j >> ${log} 2>&1 && :
-    if [ $? -ne 0 ]; then
-        ret=1
-    fi
-done
-
-if [ ${ret} -eq 0 ]; then
-    echo " PASSED" | tee -a ${log}
-else
-    echo " FAILED" | tee -a ${log}
-fi
-
-exit ${ret}
-
-# #{metaphlan_dir}/metaphlan2.py --bowtie2db ${DB} --input_type fastq --bowtie2out ${OUT_BWT2_} --nproc #{n_threads}
+# zcat #{query1_} #{query2_} | #{metaphlan_dir}/metaphlan2.py --bowtie2db ${DB} --input_type fastq --bowtie2out ${OUT_BWT2_} --nproc #{n_threads} > ${OUT_PROFILE_}
 # 08_bacterial_taxonomic_profile.rb
 
 
