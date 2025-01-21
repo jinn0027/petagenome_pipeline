57c49c513fe322b311fba6f49ebc8e405873f782
jinn0027 <jinn@zb3.so-net.ne.jp>
2025-01-21 13:50:47 +0900

    add prinseq test

--- modules/prinseq/test/t.sh
+++ modules/prinseq/test/t.sh
@@ -3,6 +3,7 @@
 fq1=../../test/s_6_1.fastq.gz
 fq2=../../test/s_6_2.fastq.gz
 
+wdir=work
 odir=results
 refdir=ref
 
@@ -10,13 +11,19 @@ log=t.log
 
 ret=0
 
-fq1=$(cd $(dirname ${fq1}) && pwd)/$(basename ${fq1})
-fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
+wdir=$(cd $(dirname ${wdir}) && pwd)/$(basename ${wdir})
 odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
 
-mkdir -p ${odir}
-/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../prinseq.sif prinseq-lite.pl -h > ${log} 2>&1
-for i in $(ls $odir/*.html)
+mkdir -p ${odir} ${wdir}
+
+wfq1=${wdir}/$(basename ${fq1} | sed 's#.gz$##')
+wfq2=${wdir}/$(basename ${fq2} | sed 's#.gz$##')
+
+gunzip ${fq1} -c > ${wfq1}
+gunzip ${fq2} -c > ${wfq2}
+
+/usr/local/bin/apptainer exec --bind ${wfq1},${wfq2},${odir} ../prinseq.sif prinseq-lite.pl -trim_right 10 -trim_left 10 -trim_qual_right 20 -trim_qual_left 20 -trim_qual_window 20 -min_len 75 -derep 1 -lc_method dust -lc_threshold 7 -trim_ns_right 1 -ns_max_n 0 -fastq ${wfq1} -fastq2 ${wfq2} -out_good ${odir}/good -out_bad ${odir}/bad > ${log} 2>&1
+for i in $(ls $odir/*.fastq)
 do
     j=$refdir/$(basename $i)
     diff -q $i $j >> ${log} 2>&1 && :
