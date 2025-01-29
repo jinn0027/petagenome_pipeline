69b22def1eb782cf3e466ed4c67cff4bbc03b434
jinn0027 <jinn@zb3.so-net.ne.jp>
2025-01-29 13:14:10 +0900

    add bcftools test

--- modules/bcftools/test/t.sh
+++ modules/bcftools/test/t.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-fq1=../../test/s_6_1.fastq.gz
-fq2=../../test/s_6_2.fastq.gz
+vcf1=../../test/idx.vcf
 
 odir=results
 refdir=ref
@@ -10,17 +9,18 @@ log=t.log
 
 ret=0
 
-fq1=$(cd $(dirname ${fq1}) && pwd)/$(basename ${fq1})
-fq2=$(cd $(dirname ${fq2}) && pwd)/$(basename ${fq2})
+vcf1=$(cd $(dirname ${vcf1}) && pwd)/$(basename ${vcf1})
 odir=$(cd $(dirname ${odir}) && pwd)/$(basename ${odir})
 
 mkdir -p ${odir}
 rm -rf ${odir}/*
 
-/usr/local/bin/apptainer exec --bind ${fq1},${fq2},${odir} ../bcftools.sif bcftools -h > ${log} 2>&1
+# get statics
+/usr/local/bin/apptainer exec --bind ${vcf1},${odir} ../bcftools.sif sh -c "\
+    bcftools stats ${vcf1} > ${odir}/stats.txt" > ${log} 2>&1
 
 failed=""
-for i in $(ls ${refdir}/*.fa)
+for i in $(ls ${refdir}/*.txt)
 do
     j=${odir}/$(basename $i)
     diff -q $i $j >> ${log} 2>&1 && :
