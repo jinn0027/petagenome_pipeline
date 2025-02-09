3472c2e9e94db1377fff6f46c23adf0460c4e958
ishikawa <ishikawa@pezy.co.jp>
2025-02-09 14:43:52 +0900

    .

--- modules/testall.sh
+++ modules/testall.sh
@@ -3,6 +3,9 @@
 rm -f testall.log
 for i in $(ls)
 do
+    if [ ! -d $i/$i.sbx ] ; then
+        continue
+    fi
     if [ $i = "common" ] || [ $i == "test" ] || [ $i == "tmp" ] ; then
         continue
     fi
@@ -11,7 +14,7 @@ do
         echo "$i : SKIPPED" | tee -a testall.log
         continue
     fi
-    if [ -d $i/$i.sbx ] && [ -f $i/test/t.sh ] && [ -d $i/test/ref ] ; then
+    if [ -f $i/test/t.sh ] && [ -d $i/test/ref ] ; then
         pushd $i/test >& /dev/null
         echo -n "$i : " | tee -a ../../testall.log
         ./t.sh | tee -a ../../testall.log
--- modules/testclear.sh
+++ modules/testclear.sh
@@ -2,12 +2,15 @@
 
 for i in $(ls)
 do
+    if [ ! -d $i/$i.sbx ] ; then
+        continue
+    fi
     if [ $i = "common" ] || [ $i == "test" ] || [ $i == "tmp" ] ; then
-	continue
+        continue
     fi
     if [ -f $i/test/c.sh ] ; then
-	pushd $i/test >& /dev/null
-	./c.sh
-	popd >& /dev/null
+        pushd $i/test >& /dev/null
+        ./c.sh
+        popd >& /dev/null
     fi
 done
