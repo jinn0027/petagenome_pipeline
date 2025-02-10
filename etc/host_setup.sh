651cec887ee9dd1eada529ebf2bb122226c6a227
ishikawa <ishikawa@pezy.co.jp>
2025-02-10 12:54:06 +0900

    .

--- etc/host_setup.sh
+++ etc/host_setup.sh
@@ -1,70 +1,86 @@
 #!/bin/bash
 
 sudo dnf -y groupinstall 'Development Tools'
-sudo dnf -y install openssl-devel libuuid-devel libseccomp-devel \
-                    wget squashfs-tools glib2-devel-2.68.4-14.el9_4.1.x86_64 \
-                    fuse3-devel-3.10.2-9.el9.x86_64 \
-                    fakeroot \
-                    cryptsetup \
-                    libsubid-dev \
-                    pigz-2.5-4.el9 \
-#                    wget git
+sudo dnf -y install \
+     openssl-devel \
+     libuuid-devel \
+     libseccomp-devel \
+     wget \
+     git \
+     squashfs-tools \
+     glib2-devel \
+     fuse3-devel \
+     fakeroot \
+     cryptsetup \
+     squashfuse \
+     libsubid-dev \
+     pigz
 
-sudo dnf --enablerepo=devel install -y shadow-utils-subid-devel
+sudo dnf --enablerepo=devel install -y \
+     shadow-utils-subid-devel
 
 # apptainer/singularityにgo必要なのでインストール
-if [ ! -d /usr/local/go ] ; then
-    if [ ! -f go1.23.3.linux-amd64.tar.gz ] ; then
-        wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
+if ! (type "go" > /dev/null 2>&1); then
+    if [ ! -d /usr/local/go ] ; then
+        if [ ! -f go1.23.3.linux-amd64.tar.gz ] ; then
+            wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
+        fi
+        sudo tar -I pigz -C /usr/local -xf go1.23.3.linux-amd64.tar.gz
     fi
-    sudo tar -I pigz -C /usr/local -xf go1.23.3.linux-amd64.tar.gz
+    export PATH=/usr/local/go/bin:$PATH
 fi
-export PATH=/usr/local/go/bin:$PATH
 
 # apptainerインストール
-if [ ! -d apptainer ] ; then
-    git clone https://github.com/apptainer/apptainer.git
+if ! (type "apptainer" > /dev/null 2>&1); then
+    if [ ! -d apptainer ] ; then
+        git clone https://github.com/apptainer/apptainer.git
+    fi
+    cd apptainer
+    ./mconfig
+    make -C ./builddir
+    sudo make -C ./builddir install
+    cd ..
 fi
-cd apptainer
-./mconfig
-make -C ./builddir
-sudo make -C ./builddir install
-cd ..
 
 # singularityインストール
-
-#if [ ! -d singularity ] ; then
-#    git clone https://github.com/sylabs/singularity.git -b v4.2.2 --recursive
+#if ! (type "singularity" > /dev/null 2>&1); then
+#    if [ ! -d singularity ] ; then
+#        git clone https://github.com/sylabs/singularity.git -b v4.2.2 --recursive
+#    fi
+#    cd singularity
+#    ./mconfig
+#    make -C ./builddir
+#    sudo make -C ./builddir install
+#    cd ..
 #fi
-#cd singularity
-#./mconfig
-#make -C ./builddir
-#sudo make -C ./builddir install
-#cd ..
 
 # nextflowにjava必要なのでインストール
-
-curl -s https://get.sdkman.io | bash
-source ~/.sdkman/bin/sdkman-init.sh
-sdk install java 17.0.10-tem
-java --version
-#openjdk 17.0.10 2024-01-16
-#OpenJDK Runtime Environment Temurin-17.0.10+7 (build 17.0.10+7)
-#OpenJDK 64-Bit Server VM Temurin-17.0.10+7 (build 17.0.10+7, mixed mode, sharing)
+if ! (type "java" > /dev/null 2>&1); then
+    curl -s https://get.sdkman.io | bash
+    source ~/.sdkman/bin/sdkman-init.sh
+    sdk install java 17.0.10-tem
+    java --version
+    #openjdk 17.0.10 2024-01-16
+    #OpenJDK Runtime Environment Temurin-17.0.10+7 (build 17.0.10+7)
+    #OpenJDK 64-Bit Server VM Temurin-17.0.10+7 (build 17.0.10+7, mixed mode, sharing)
+fi
 
 # nextflowインストール
-curl -s https://get.nextflow.io | bash
-chmod +x nextflow
-
-sudo mv nextflow /usr/local/bin
-nextflow -v
-#nextflow version 24.10.2.5932
+if ! (type "nextflow" > /dev/null 2>&1); then
+    curl -s https://get.nextflow.io | bash
+    chmod +x nextflow
+    sudo mv nextflow /usr/local/bin
+    nextflow -v
+    #nextflow version 24.10.2.5932
+fi
 
 ###############################################
 
-sudo dnf -y install squashfuse
+# SElinuxをpermissiveに設定。
+# これをやらないとapptainerでfakerootでdnfをやろうとするとエラーが発生する。
 sudo setenforce 0
 
+# apptainerのキャッシュ先を/dev/shmに
 mkdir -p /dev/shm/.apptainer
 export APPTAINER_CACHEDIR=/dev/shm/.apptainer
 
