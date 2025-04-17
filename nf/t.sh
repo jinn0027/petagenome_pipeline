cd1790a21ec5895548c3bdff955a1138db32102b
ishikawa <ishikawa@pezy.co.jp>
2025-04-18 02:49:54 +0900

    .

--- nf/t.sh
+++ nf/t.sh
@@ -4,4 +4,4 @@
 
 #nextflow run fastp.nf --test_fastp_reads "../modules/test/s_6_{1,2}.fastq.gz" --threads $(nproc) --petagenomeDir=$(pwd)/..
 
-nextflow run spades.nf --test_spades_reads "../modules/test/ecoli_1K_{1,2}.fq.gz" --threads 4 --petagenomeDir=$(pwd)/..
+nextflow run spades.nf --test_spades_reads "../modules/test/ecoli_1K_{1,2}.fq.gz" --threads $(nproc) --petagenomeDir=$(pwd)/..
