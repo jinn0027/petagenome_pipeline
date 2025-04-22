a883d89192c2c79d5a82e58392765b69fa48a736
ishikawa <ishikawa@pezy.co.jp>
2025-04-22 14:17:10 +0900

    .

--- nf/t.sh
+++ nf/t.sh
@@ -16,7 +16,9 @@
 
 #nextflow run megahit.nf --test_megahit_reads "../modules/test/ecoli_1K_{1,2}.fq.gz" --threads $(nproc) --petagenomeDir=$(pwd)/..
 
-nextflow run blast.nf --test_blast_ref "../modules/test/ecoli_1K_1.fa.gz" --test_blast_qry "../modules/test/q1.fasta" --petagenomeDir=$(pwd)/..
+#nextflow run blast.nf --test_blast_ref "../modules/test/ecoli_1K_1.fa.gz" --test_blast_qry "../modules/test/q1.fasta" --petagenomeDir=$(pwd)/..
+
+nextflow run diamond.nf --test_diamond_ref "../modules/test/1.faa" --test_diamond_qry "../modules/test/2.faa" --petagenomeDir=$(pwd)/..
 
 ################################# NG
 #nextflow run spades.nf --test_spades_reads "../modules/test/s_6_{1,2}.fastq.gz" --threads $(nproc) --petagenomeDir=$(pwd)/..
