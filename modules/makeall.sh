aded6c141d636cfd2b1cbb97333c82afca0f3c06
ishikawa <ishikawa@pezy.co.jp>
2025-07-31 16:57:35 +0900

    .

--- modules/makeall.sh
+++ modules/makeall.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+NEED_MODULES=$(pwd)/../etc/need_modules.txt
+
+function check_module () {
+    module=$1
+    if [ ! -f ${NEED_MODULES} ] || grep -w -q ${module} ${NEED_MODULES} ; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 rm -f makeall.log
 
-# SElinuxをpermissiveに設定。
-# これをやらないとapptainerでfakerootでdnfをやろうとするとエラーが発生する。
-if [ "$(getenforce)" != "Permissive" ] && [ "$(getenforce)" != "Disabled" ] ; then
-    sudo setenforce 0
+if groups | grep -q wheel ; then
+    # SElinuxをpermissiveに設定。
+    # これをやらないとapptainerでfakerootでdnfをやろうとするとエラーが発生することがある。
+    if [ "$(getenforce)" != "Permissive" ] && [ "$(getenforce)" != "Disabled" ] ; then
+        sudo setenforce 0
+    fi
 fi
 
 make -j$(nproc) -C common all
@@ -15,6 +28,11 @@ do
     if [ $i = "common" ] || [ $i == "test" ] || [ $i == "tmp" ] ; then
         continue
     fi
+    
+    if ! check_module $i ; then
+	continue
+    fi
+    
     if [ -d $i ] ; then
         echo "$(date) $i" | tee -a makeall.log
 	    make -j$(nproc) -C $i all
