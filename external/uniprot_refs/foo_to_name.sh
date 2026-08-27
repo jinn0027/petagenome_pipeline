35600d182fda7d31bd0a1fea8b60bac24b485673
ishikawa <ishikawa@pezy.co.jp>
2026-08-27 20:46:27 +0900

    .

--- external/uniprot_refs/foo_to_name.sh
+++ external/uniprot_refs/foo_to_name.sh
@@ -6,50 +6,55 @@ if [ ! -f go-basic.obo ] ; then
     wget https://purl.obolibrary.org/obo/go/go-basic.obo
 fi
 
-awk '
-BEGIN { id=""; name=""; namespace="" }
-/^\[Term\]/ { id=""; name=""; namespace="" }
-/^id: / { id=$2 }
-/^name: / { 
-    # "name: " 以降の文字列をすべて取得する
-    name=$0; 
-    sub(/^name: /, "", name);
-}
-/^namespace: / { 
-    namespace=$2; 
-    # タームのブロックが終わったら出力
-    if (id != "" && name != "") {
-        print id "\t" name "\t" namespace;
-    }
-}
-' go-basic.obo > go_to_name.tsv
-
-exit
+if [ ! -f go_to_name.tsv ] ; then
+    awk '
+        BEGIN { id=""; name=""; namespace="" }
+        /^\[Term\]/ { id=""; name=""; namespace="" }
+        /^id: / { id=$2 }
+        /^name: / { 
+            # "name: " 以降の文字列をすべて取得する
+            name=$0; 
+            sub(/^name: /, "", name);
+        }
+        /^namespace: / { 
+            namespace=$2; 
+            # タームのブロックが終わったら出力
+            if (id != "" && name != "") {
+                print id "\t" name "\t" namespace;
+            }
+        }' go-basic.obo > go_to_name.tsv
+fi
 
 ### taxid_to_name.tsv
 
-if [ ! -f taxdump.tar.gz ]; then
+if [ ! -f taxdump.tar.gz ] ; then
     wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
 fi
+
+if [ ! -f taxid_to_name.tsv ] ; then
+
+    tar -zxf taxdump.tar.gz names.dmp
     
-tar -zxf taxdump.tar.gz names.dmp
+    # "scientific name" の行だけを抽出し、taxid と name の 2 カラム TSV に変換
+    # names.dmp のフォーマット: tax_id\t|\tname_txt\t|\tunique name\t|\tname class\t|
+    awk -F'\t\\|\t' 'BEGIN{OFS="\t"} $4 ~ /^scientific name/ {
+    	gsub(/\t\|/, "", $4);
+    	print $1, $2
+	}' names.dmp > taxid_to_name.tsv
     
-# "scientific name" の行だけを抽出し、taxid と name の 2 カラム TSV に変換
-# names.dmp のフォーマット: tax_id\t|\tname_txt\t|\tunique name\t|\tname class\t|
-awk -F'\t\\|\t' 'BEGIN{OFS="\t"} $4 ~ /^scientific name/ {
-    gsub(/\t\|/, "", $4);
-    print $1, $2
-}' names.dmp > taxid_to_name.tsv
-    
-rm -f names.dmp
+    rm -f names.dmp
+fi
 
 ### react_to_name.tsv
+
 # dl page:   https://reactome.org/download-data
 if [ ! -f ReactomePathways.txt ] ; then
     wget https://download.reactome.org/97/ReactomePathways.txt
 fi
 
-awk -F '\t' '{print($1 "\t" $2)}' ReactomePathways.txt > react_to_name.tsv
+if [ ! -f react_to_name.tsv ] ; then
+    awk -F '\t' '{print($1 "\t" $2)}' ReactomePathways.txt > react_to_name.tsv
+fi
 
 ### tcdb_to_name.tsv
 
@@ -57,7 +62,9 @@ if [ ! -f tcdb_families.tsv ] ; then
     curl -s "https://www.tcdb.org/cgi-bin/projectv/public/families.py" > tcdb_families.tsv
 fi
 
-awk -F '\t' '{print($1 "\t" $2)}' tcdb_families.tsv > tcdb_to_name.tsv
+if [ ! -f tcdb_to_name.tsv ] ; then
+    awk -F '\t' '{print($1 "\t" $2)}' tcdb_families.tsv > tcdb_to_name.tsv
+fi
 
 ### ko_to_name.tsv
 
@@ -65,18 +72,16 @@ if [ ! -f e7.og_info_kegg_go.tsv.gz ] ; then
     wget https://eggnogdb.org/public/eggnog7/e7.og_info_kegg_go.tsv.gz
 fi
 
-
-zcat e7.og_info_kegg_go.tsv.gz | awk -F'\t' '{
-    # $6がタンパク質リスト、$7がKO、$8がKO名
-    proteins = $6;
-    ko = $7;
-    name = $8;
+if [ ! -f ko_to_name.tsv ] ; then
+    zcat e7.og_info_kegg_go.tsv.gz | awk -F'\t' '{
+        # $6がタンパク質リスト、$7がKO、$8がKO名
+        proteins = $6;
+        ko = $7;
+        name = $8;
     
-    if (proteins != "" && ko != "") {
-        n = split(proteins, p_arr, ",");
-        for (i=1; i<=n; i++) {
+        if (ko != "" && name != "") {
             print ko "\t" name > "ko_to_name.tsv";
-        }
-    }
-}'
+   	}
+    }'
+fi
 
--- external/uniprot_refs/uniprot_to_foo.sh
+++ external/uniprot_refs/uniprot_to_foo.sh
@@ -8,52 +8,58 @@ if [ ! -f idmapping.dat.gz ] ; then
     wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz
 fi
 
-zcat idmapping.dat.gz | awk -F'\t' '
-{
-    ac = $1;
-    type = $2;
-    val = $3;
+if [ ! -f uniprot_to_tcdb.tsv ] ; then
+    
+    zcat idmapping.dat.gz | awk -F'\t' '
+    {
+        ac = $1;
+        type = $2;
+        val = $3;
 
-    if (type == "KEGG") {
-        print ac "\t" val > "uniprot_to_kegggenes.tsv";
-    }
-    else if (type == "RefSeq") {
-        print ac "\t" val > "uniprot_to_refseq.tsv";
-    }
-    else if (type == "NCBI_TaxID") {
-        print ac "\t" val > "uniprot_to_taxid.tsv";
-    }
-    else if (type == "eggNOG") {
-        print ac "\t" val > "uniprot_to_eggnog.tsv";
-    }
-    else if (type == "BioCyc") {
-        print ac "\t" val > "uniprot_to_biocyc.tsv";
-    }
-    else if (type == "Reactome") {
-        print ac "\t" val > "uniprot_to_react.tsv";
-    }
-    else if (type == "PlantReactome") {
-        print ac "\t" val > "uniprot_to_plantreact.tsv";
-    }
-    else if (type == "TCDB") {
-        print ac "\t" val > "uniprot_to_tcdb.tsv";
-    }
-    else if (type == "PHI-base") {
-        print ac "\t" val > "uniprot_to_phibase.tsv";
-    }
-    else if (type == "GuidetoPHARMACOLOGY") {
-        print ac "\t" val > "uniprot_to_pharmacology.tsv";
-    }
-}'
+        if (type == "KEGG") {
+            print ac "\t" val > "uniprot_to_kegggenes.tsv";
+        }
+        else if (type == "RefSeq") {
+            print ac "\t" val > "uniprot_to_refseq.tsv";
+        }
+        else if (type == "NCBI_TaxID") {
+            print ac "\t" val > "uniprot_to_taxid.tsv";
+        }
+        else if (type == "eggNOG") {
+            print ac "\t" val > "uniprot_to_eggnog.tsv";
+        }
+        else if (type == "BioCyc") {
+            print ac "\t" val > "uniprot_to_biocyc.tsv";
+        }
+        else if (type == "Reactome") {
+            print ac "\t" val > "uniprot_to_react.tsv";
+        }
+        else if (type == "PlantReactome") {
+            print ac "\t" val > "uniprot_to_plantreact.tsv";
+        }
+        else if (type == "TCDB") {
+            print ac "\t" val > "uniprot_to_tcdb.tsv";
+        }
+        else if (type == "PHI-base") {
+            print ac "\t" val > "uniprot_to_phibase.tsv";
+        }
+        else if (type == "GuidetoPHARMACOLOGY") {
+            print ac "\t" val > "uniprot_to_pharmacology.tsv";
+        }
+    }'
 
-awk 'BEGIN { FS="\t"; OFS="\t" } {
-    # 2列目のTC番号を "." で分割して、上位3つを取得
-    n = split($2, a, ".")
-    if (n >= 3) {
-        family_id = a[1] "." a[2] "." a[3]
-        print $1, family_id
-    }
-}' uniprot_to_tcdb.tsv > uniprot_to_tcdb_lough.tsv
+fi
+
+if [ ! -f uniprot_to_tcdb_lough.tsv ] ; then
+    awk 'BEGIN { FS="\t"; OFS="\t" } {
+        # 2列目のTC番号を "." で分割して、上位3つを取得
+        n = split($2, a, ".")
+        if (n >= 3) {
+            family_id = a[1] "." a[2] "." a[3]
+            print $1, family_id
+        }
+    }' uniprot_to_tcdb.tsv > uniprot_to_tcdb_lough.tsv
+fi
 
 ### uniprot_to_go.tsv
 
@@ -61,7 +67,61 @@ if [ ! -f goa_uniprot_all.gaf.gz ] ; then
     wget ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/goa_uniprot_all.gaf.gz
 fi
 
-zcat goa_uniprot_all.gaf.gz | awk -F'\t' '!/^!/ {print $2 "\t" $5}' | sort -u > uniprot_to_go.tsv
+if [ ! -f uniprot_to_go.tsv ] ; then
+    zcat goa_uniprot_all.gaf.gz | awk -F'\t' '!/^!/ {print $2 "\t" $5}' | sort -u > uniprot_to_go.tsv
+fi
+
+### uniprot_to_ko.tsv
+
+if [ ! -f uniprot_ids.txt ] ; then
+    zcat idmapping.dat.gz | awk '{print($1)}' | uniq > uniprot_ids.txt
+fi
+
+if [ ! -f uniprot_to_ko.tsv ] ; then
+python <<EOF
+import gzip
+import sys
+
+# 1. 1.5億行の uniprot_ids.txt をメモリ上のハッシュセットにロード
+print("Loading uniprot_ids.txt...", file=sys.stderr)
+uniprot_set = set()
+with open("./uniprot_ids.txt", "r") as f:
+  for line in f:
+    uid = line.strip()
+    if uid:
+      uniprot_set.add(uid)
+print(f"Loaded {len(uniprot_set)} IDs.", file=sys.stderr)
+
+# 2. e7.og_info_kegg_go.tsv.gz を読み込みながら突合して出力
+print("Processing e7.og_info_kegg_go.tsv.gz...", file=sys.stderr)
+output_file = "uniprot_to_ko.tsv"
+
+with gzip.open("e7.og_info_kegg_go.tsv.gz", "rt") as fin, open(
+        output_file, "w"
+) as fout:
+  for line in fin:
+    cols = line.rstrip("\n").split("\t")
+    if len(cols) < 8:
+      continue
+
+    proteins = cols[5]  # $6
+    raw_ko = cols[6]  # $7
+
+    if proteins and raw_ko:
+      ko = raw_ko.split("|")[0]
+      for p in proteins.split(","):
+        sub_parts = p.split(".")
+        if len(sub_parts) == 2:
+          prot_id = sub_parts[1]
+          # O(1) で1.5億行のセットから判定
+          if prot_id in uniprot_set:
+            fout.write(f"{prot_id}\t{ko}\n")
+
+print("Done!", file=sys.stderr)
+EOF
+
+fi
 
 echo "抽出が完了しました。生成されたファイル一覧:"
 ls -lh uniprot_to_*.tsv
+
